c75ab992da92dd248af097cf535c6222fb26b0fb
NoTra <aerandir16@gmail.com>
2024-01-22 13:55:46 +0100

    wip materials

---
+++ Assets/MaterialLerp.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MaterialLerp : MonoBehaviour
+{
+    private List<Material> _sadMaterials = new List<Material>();
+    [SerializeField] private List<Material> _happyMaterials = new List<Material>();
+
+    // Slider de 0 à 1
+    [SerializeField] [Range(0f, 1f)] private float _lerpValue = 0f;
+
+    private MeshRenderer _meshRenderer;
+    private Shader _standardShader;
+
+    private bool _canLerp = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lerpValue = 0f;
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+
+        // Récupérez les matériaux initiaux du Renderer
+        _sadMaterials = _meshRenderer.materials.ToList();
+
+        for (int i = 0; i < _sadMaterials.Count; i++)
+        {
+            Debug.Log("Sad " + i + ": " + _sadMaterials[i].name);
+        }
+
+        Debug.Log("Happy materials: " + _happyMaterials.Count);
+
+        // Assurez-vous que les deux ensembles de matériaux ont la même longueur
+        if (_sadMaterials.Count != _happyMaterials.Count)
+        {
+            Debug.LogError("Les ensembles de matériaux n'ont pas la même longueur.");
+            _canLerp = false;
+        }
+        
+        _standardShader = Shader.Find("Standard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_canLerp) { return; }
+
+        LerpMaterials();
+    }
+
+    void LerpMaterials()
+    {
+        // On fait une copie des matériaux du Renderer
+        Material[] lerpedMaterials = new Material[_sadMaterials.Count];
+        Debug.Log(lerpedMaterials);
+
+        for (int i = 0; i < _sadMaterials.Count; i++)
+        {
+            Debug.Log("Lerp " + i);
+            lerpedMaterials[i] = new Material(_standardShader);
+
+            // Interpolez entre les matériaux de chaque ensemble
+            lerpedMaterials[i].Lerp(
+                _sadMaterials[i],
+                _happyMaterials[i],
+                _lerpValue
+            );
+        }
+
+        // Appliquez les matériaux lerpés au Renderer du GameObject
+        _meshRenderer.materials = lerpedMaterials;
+
+        _canLerp = false;
+    }
+}
