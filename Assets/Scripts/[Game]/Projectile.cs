9e56bf24ea5e61358a5b5a08fdc0d1194df4c6e1
DournauxNathan <dournauxnathan@gmail.com>
2024-01-22 14:08:59 +0100

    Shoot & Projectiles

---
+++ Assets/Scripts/[Game]/Projectile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float damage = 10f;
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        // Destroy the projectile after its lifetime expires
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // You can customize the logic for what happens when the projectile collides with something
+        // For now, we'll just destroy the projectile
+        //Destroy(gameObject);
+    }
+}
