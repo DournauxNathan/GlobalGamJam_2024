5508335872ae11d26604dbb5d82198b664e8bde5
DournauxNathan <dournauxnathan@gmail.com>
2024-01-22 12:57:28 +0100

    Mouse Lock

--- Assets/Scripts/[Game]/PlayerMovement.cs
+++ Assets/Scripts/[Game]/PlayerMovement.cs
@@ -6,13 +6,36 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
 
+    private CharacterController controller;
+
+    private Vector3 inputVector;
+    private float gravityModifier = -10f;
+    private Vector3 movement;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        GetInput();
+        Move();
+    }
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
-        transform.Translate(movement);
+    Vector3 GetInput()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        input.Normalize();
+        input = transform.TransformDirection(input);
+
+        return movement = (input * speed) + (Vector3.up * gravityModifier);
+    }
+
+    void Move()
+    {
+        controller.Move(movement * Time.deltaTime);
     }
 }
+
 
