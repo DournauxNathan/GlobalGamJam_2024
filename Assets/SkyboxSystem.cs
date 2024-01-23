20b40177e71c05c05ba3d659b818a8a667e6b740
NoTra <aerandir16@gmail.com>
2024-01-23 21:58:10 +0100

    WIP

---
+++ Assets/SkyboxSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Rendering;
+using static System.TimeZoneInfo;
+
+public class SkyboxSystem : MonoBehaviour
+{
+    public Material _skyboxHappy;
+    public Material _skyboxSad;
+
+    public float _transitionTime = 2f;
+
+    public PlayerController _playerController;
+
+    [Range(0f, 1f)] public float testCompletion = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_t", _playerController._currentZone._completion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // DynamicGI.UpdateEnvironment();
+
+        // RenderSettings.skybox = Mathf.Lerp(_skyboxSad, _skyboxHappy, testCompletion);// _playerController._currentZone._completion);
+        // LerpSkyboxMaterials(_skyboxSad, _skyboxHappy, testCompletion);// _playerController._currentZone._completion);
+        /*if (testCompletion < 1)
+        {
+            testCompletion += Time.deltaTime / _transitionTime;
+            RenderSettings.skybox.Lerp(_skyboxSad, _skyboxHappy, testCompletion);
+            DynamicGI.UpdateEnvironment(); // Mettez à jour l'éclairage global si nécessaire
+        }*/
+    }
+
+    public void LerpSkybox()
+    {
+        float elapsedTime = 0f;
+
+        float startT = RenderSettings.skybox.GetFloat("_t");
+        float endT = _playerController._currentZone._completion;
+
+        while (elapsedTime < _transitionTime)
+        {
+            float t = elapsedTime / _transitionTime;
+
+            RenderSettings.skybox.SetFloat("_t", Mathf.Lerp(startT, endT, t));
+
+            elapsedTime += Time.deltaTime;
+        }
+
+        RenderSettings.skybox.SetFloat("_t", endT);
+    }
+}
