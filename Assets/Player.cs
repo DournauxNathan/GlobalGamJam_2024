993f9738761fb257ac3991c9b447306d0e561c18
NoTra <aerandir16@gmail.com>
2024-01-23 15:21:46 +0100

    feat(sfx rain) : ajout rain en fonction de la completion du quartier courant

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.VFX;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player : MonoBehaviour
 {
     public ZoneManager _currentZone;
+    public VisualEffect _rainEffect;
 
+    private void Awake()
+    {
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +22,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void UpdateRain()
+    {
+        Debug.Log("Update rain !");
+        if (_rainEffect != null)
+        {
+            Debug.Log("Rain effect modifié (" + _currentZone.name + ") : " + Mathf.Abs(_currentZone._completion * 800 - 800));
+            _rainEffect.SetFloat("Rain Intensity", Mathf.Abs(_currentZone._completion * 800 - 800));
+        } else
+        {
+            Debug.Log("Rain effect est introuvable !");
+        }
         
     }
 }
