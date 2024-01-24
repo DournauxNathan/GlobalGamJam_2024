e2e00286e21f5265ba5c54c049349353e1d76865
DournauxNathan <dournauxnathan@gmail.com>
2024-01-24 03:29:39 +0100

    Update Mr Smith

--- Assets/Scripts/[Game]/FacePlayer.cs
+++ Assets/Scripts/[Game]/FacePlayer.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class FacePlayer : MonoBehaviour
 {
-    private void Update()
+    private void LateUpdate()
     {
         // Ensure the Camera.main is not null before trying to access it
         if (Camera.main != null)
         {
             // Rotate the canvas to face the player
-            transform.LookAt(Camera.main.transform.position, Vector3.up);
+            transform.LookAt(Camera.main.transform.position, -Vector3.up);
 
-            // Optionally, you can constrain the rotation to only affect the y-axis
-            // transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         }
     }
 }
