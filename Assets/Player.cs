03041e473028938475f86696cdeb534f52713f65
NoTra <aerandir16@gmail.com>
2024-01-24 01:27:05 +0100

    Lib break

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.VFX;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player : MonoBehaviour
 {
--- Assets/SkyboxSystem.cs
+++ Assets/SkyboxSystem.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.Rendering;
-using static System.TimeZoneInfo;
 
 public class SkyboxSystem : MonoBehaviour
 {
